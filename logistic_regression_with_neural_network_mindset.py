51dcb32f1e63b858d36198d510ad30e46ec51212
achoo486 <saliraza486@gmail.com>
2019-02-28 00:21:21 +0500

    Implemented model fitting/training function for single image (observing overfitting
    Observing overfitting in the results. Have written the code for weight and bias clipping in comments in calculate_derivates function.

--- logistic_regression_with_neural_network_mindset.py
+++ logistic_regression_with_neural_network_mindset.py
@@ -12,17 +12,34 @@ from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
-#%% Loading the data (cat/non-cat)
-# =============================================================================
-# train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
-# =============================================================================
-
-#%% Example of a picture
-# =============================================================================
-# index = 13
-# plt.imshow(train_set_x_orig[index])
-# print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
-# =============================================================================
+#%% Function for predicting correctly a single image
+# inputs:
+    # -image --> image to fit model to, of shape (height, width, number of channels)
+    # -epochs --> number of iterations for updating parameters, an integer
+    # -learning_rate --> learning rate for updation of weights and bias
+    # -seed --> seed value for random function. Default is None (for random), an integer
+    # -true_output --> true label of image, an integer either 0 or 1
+# outputs:
+    # -parameters_dictionary --> final parameters dictionary after model fitting,
+    #                            {'weights': weights matrix of shape (number of image nodes or pixels, 1),
+    #                            'bias': bias, a float}
+def train_on_single_image(image, epochs=1000, learning_rate=0.01, seed=None, true_output=1):
+    linearized_image = linearize_image(image)
+    num_of_weights = linearized_image.shape[0]
+    
+    np.random.seed(seed)
+    weights = np.random.uniform(low=-1, high=1, size=(num_of_weights, 1)) * 0.001
+    bias = np.random.rand() * 0.001
+    parameters_dictionary = {'weights': weights, 'bias': bias}
+    
+    for i in range(epochs):
+        network_output = logistic_unit_output(image, parameters_dictionary)
+        derivatives_dictionary = calculate_derivatives(image, network_output, true_output)
+        parameters_dictionary = update_parameters(parameters_dictionary, derivatives_dictionary, learning_rate)
+        if np.mod(i, 100) == 0:
+            print('epoch = ', i, ' Network Output =', network_output, ', Loss = ', calculate_loss(network_output, true_output))
+            
+    return parameters_dictionary
 
 #%%Function to update parameters (weights and bias)
 # input:
@@ -56,7 +73,7 @@ def update_parameters(parameters_dictionary, derivatives_dictionary, learning_ra
     # -true_output --> actuaL output, a float
 # output: 
     # -derivatives dictionary, {'dw': derivatives of Loss w.r.t weights, of shape (number of weights, 1),
-    # 'db': derivative of Loss w.r.t bias, a float}
+    #                          'db': derivative of Loss w.r.t bias, a float}
     
 def calculate_derivatives(network_input, network_output, true_output):
     # derivative of Loss w.r.t z, a number
@@ -69,6 +86,12 @@ def calculate_derivatives(network_input, network_output, true_output):
     
     #derivate of Loss w.r.t bias
     db = dz
+    
+# =============================================================================
+#     dw[np.abs(dw) > 1e-1] = 0
+#     if db > 1e-1:
+#         db = 0
+# =============================================================================
     
     derivatives_dictionary = {'dw': dw, 'db': db}
     return derivatives_dictionary
@@ -89,60 +112,57 @@ def sigmoid(x):
     # image --> image of shape (height, width, num_of_channels) 
 # output: linear image of shape (height * width * num_of_channels)
 def linearize_image(image):
-    #%% Extract dimensions of input image
+    # Extract dimensions of input image
     image_height = image.shape[0]
     image_width = image.shape[1]
     image_channels= image.shape[2]
     
-    #%% Linearize image
+    # Linearize image
     linear_image = image.reshape([image_height * image_width * image_channels])
     linear_image = np.expand_dims(linear_image, axis=1)
     return linear_image
     
 #%% Function for calculating output of Logistic Regression unit
 # input:
-    # image --> image of shape (height, width, num_of_channels)
+    # -image --> image of shape (height, width, num_of_channels)
     # -parameters_dictionary --> dictionary having {'weights': weights of shape (number of weights, 1), 
     #                                              'bias': bias, a number}
     
 # output:
-    # logistic unit output, a number
+    # -logistic unit output, a number
 def logistic_unit_output(image, parameters_dictionary):
     weights = parameters_dictionary['weights']
     bias = parameters_dictionary['bias']
     
     linear_image = linearize_image(image)
 
-    #%% Weights transposed for multiplication
+    # Weights transposed for multiplication
     # (number_of_weights, 1) --> (1, number_of_weights)
     weights_transposed = weights.T
     
-    #%% Multiply weights with input and add bias
+    # Multiply weights with input and add bias
     # Note that bias is a float but gets broadcasted to a higher dimensional matrix for addition
     output = np.matmul(weights_transposed, linear_image) + bias
     
-    #%% Squeezing output of shape (1, 1) to a single float number
+    # Squeezing output of shape (1, 1) to a single float number
     output = np.squeeze(output)
     
-    #%% Take sigmoid of output
+    # Take sigmoid of output
     sigmoid_output = sigmoid(output)
     
     return sigmoid_output
 
-#%% check function
-print('calculate_loss(0.85, 1) = ', calculate_loss(0.85, 1))
 
-#%% Checking calculate_derivates function
-image = np.array([[[1, 2], [3, 4]], 
-                  [[5, 6], [7, 8]],
-                  [[9, 10], [11, 12]]])
+#%% Main Code
+    
+#%% Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
-weights = np.array([[0.0012, 0.0011, 0.0010, 0.009, 0.008, 0.007, 0.006, 0.005, 0.004, 0.003, 0.002, 0.001]]).T
-bias = 2.5
-parameters_dicionary = {'weights': weights, 'bias': bias}
-logistic_output = logistic_unit_output(image, parameters_dicionary)
-derivatives_dictionary = calculate_derivatives(image, logistic_output, 1)
+#%% Example of a picture
+index = 13
+plt.imshow(train_set_x_orig[index])
+print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
 
-print('logistic_output = ', logistic_output)
-print('dw = ', derivatives_dictionary['dw'])
-print('db = ', derivatives_dictionary['db'])
+image = train_set_x_orig[index]
+
+train_on_single_image(image, epochs=10000, learning_rate=1e-6, seed=1, true_output=1)
