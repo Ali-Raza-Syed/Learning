f974a5e6f18dc5a25837392f66609f8c0da556f2
achoo486 <saliraza486@gmail.com>
2019-04-30 22:39:59 +0500

    Completed functions for multiple images being trained and tested on single node network
    Image data set is not being vectorized.

--- logistic_regression_with_neural_network_mindset.py
+++ logistic_regression_with_neural_network_mindset.py
@@ -12,32 +12,40 @@ from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
-#%% Function for predicting correctly a single image
+#%% Function for training on multiple images
 # inputs:
-    # -image --> image to fit model to, of shape (height, width, number of channels)
+    # -images --> images to fit model to, of shape (number of images, height, width, number of channels)
+    # -labels --> images labels of shape (1, number of images)
     # -epochs --> number of iterations for updating parameters, an integer
     # -learning_rate --> learning rate for updation of weights and bias
     # -seed --> seed value for random function. Default is None (for random), an integer
-    # -true_output --> true label of image, an integer either 0 or 1
+    # -print_loss_flag --> whether to print loss or not, bool
+    # -print_after_epochs --> number of epochs after which to print the loss
 # outputs:
     # -parameters_dictionary --> final parameters dictionary after model fitting,
     #                            {'weights': weights matrix of shape (number of image nodes or pixels, 1),
-    #                            'bias': bias, a float}
-def train_on_single_image(image, epochs=1000, learning_rate=0.01, seed=None, true_output=1, print_loss=False):
-    linearized_image = linearize_image(image)
-    num_of_weights = linearized_image.shape[0]
-    
+    #                            'bias': bias, a float}    
+def train_on_multiple_images(images, labels, epochs=1000, learning_rate=0.01, seed=None, print_loss_flag=False, print_after_epochs=100):
+    num_of_weights = images.shape[1] * images.shape[2] * images.shape[3]
     np.random.seed(seed)
-    weights = np.random.uniform(low=-1, high=1, size=(num_of_weights, 1)) * 0.001
-    bias = np.random.rand() * 0.001
+    weights = np.random.uniform(low=-1, high=1, size=(num_of_weights, 1)) * 1
+    bias = np.random.rand() * 1
     parameters_dictionary = {'weights': weights, 'bias': bias}
-    
-    for i in range(epochs):
-        network_output = logistic_unit_output(image, parameters_dictionary)
-        derivatives_dictionary = calculate_derivatives(image, network_output, true_output)
-        parameters_dictionary = update_parameters(parameters_dictionary, derivatives_dictionary, learning_rate)
-        if np.mod(i, 100) == 0 and print_loss:
-            print('epoch = ', i, ' Network Output =', network_output, ', Loss = ', calculate_loss(network_output, true_output))
+    num_of_images = images.shape[0]
+    for epoch_index in range(epochs):
+        averaged_derivatives_dictionary = {'dw': np.zeros([num_of_weights, 1]), 'db': 0}
+        for image_index in range(num_of_images):
+            current_image = images[image_index]
+            network_output = logistic_unit_output(current_image, parameters_dictionary)    
+            derivatives_dictionary = calculate_derivatives(current_image, network_output, labels[:, image_index])
+            averaged_derivatives_dictionary['dw'] += derivatives_dictionary['dw']
+            averaged_derivatives_dictionary['db'] += derivatives_dictionary['db']
+        
+        averaged_derivatives_dictionary['dw'] /= num_of_images
+        averaged_derivatives_dictionary['db'] /= num_of_images
+        parameters_dictionary = update_parameters(parameters_dictionary, averaged_derivatives_dictionary, learning_rate)
+        if np.mod(epoch_index, print_after_epochs) == 0 and print_loss_flag:
+            print('epoch = ', epoch_index, '\nLoss = ', calculate_loss_multiple_images(images, parameters_dictionary, labels))
             
     return parameters_dictionary
 
@@ -71,13 +79,15 @@ def update_parameters(parameters_dictionary, derivatives_dictionary, learning_ra
     # -network_input --> network input of shape (height, width, num_of_channels)
     # -network_output --> output of network, a float
     # -true_output --> actuaL output, a float
+    # -threshold_flag --> whether to apply threshold on calculated derivative values or not, a bool
+    # -threshold --> threshold to be applied on calculated derivative values
 # output: 
     # -derivatives dictionary, {'dw': derivatives of Loss w.r.t weights, of shape (number of weights, 1),
     #                          'db': derivative of Loss w.r.t bias, a float}
     
-def calculate_derivatives(network_input, network_output, true_output):
+def calculate_derivatives(network_input, network_output, true_output, threshold_flag=False, threshold=1e-1):
     # derivative of Loss w.r.t z, a number
-    dz = network_output - true_output
+    dz = network_output - np.squeeze(true_output)
 
     linearized_network_input = linearize_image(network_input)
     
@@ -87,18 +97,31 @@ def calculate_derivatives(network_input, network_output, true_output):
     #derivate of Loss w.r.t bias
     db = dz
     
-    dw[np.abs(dw) > 1e-1] = 0
-    if db > 1e-1:
-        db = 0
+    #thresholding
+    if threshold_flag:
+        dw[np.abs(dw) > threshold] = 0
+        if db > threshold:
+            db = 0
     
     derivatives_dictionary = {'dw': dw, 'db': db}
     return derivatives_dictionary
 
 #%% Loss function implementation
-# network_output --> network output
-# true_output --> actual output
-def calculate_loss(network_output, true_output):
-    return -((true_output * np.log(network_output)) + ((1 - true_output) * np.log(1 - network_output)))
+# input:
+    # network_output --> network output
+    # true_output --> actual output
+# output: loss, a float
+def calculate_loss_multiple_images(images, parameters_dictionary, labels):
+    loss = 0
+    num_of_images = images.shape[0]
+    for image_index in range(num_of_images):
+        network_output = logistic_unit_output(images[image_index], parameters_dictionary)
+        network_output = 1e-5 if network_output == 0 else network_output
+        network_output = 1-1e-5 if network_output == 1 else network_output
+        true_output = np.squeeze(labels[:, image_index])
+        loss += -((true_output * np.log(network_output)) + ((1 - true_output) * np.log(1 - network_output)))
+        
+    return loss/num_of_images
 
 #%% Sigmoid function implementation
 # x --> number to calculare sigmoid of
@@ -150,16 +173,29 @@ def logistic_unit_output(image, parameters_dictionary):
     
     return sigmoid_output
 
-#%%Function to test model
+#%%Function to test model on  multiple images
 # input:
-    # -image --> image of shape (height, width, num_of_channels)
+    # -images --> images of shape (image number, height, width, num_of_channels)
+    # -labels --> image labels of shape (1, number of images)
     # -parameters_dictionary --> dictionary having {'weights': weights of shape (number of weights, 1), 
     #                                              'bias': bias, a number}
     
 # output:
     # -logistic unit output, a number
-def test_model(image, parameters_dictionary):
-    return logistic_unit_output(image, parameters_dictionary)
+def test_model_multiple_images(images, labels, parameters_dictionary):
+    num_of_images = images.shape[0]
+    correct_predictions = 0
+    for image_index in range(num_of_images):
+        current_image = images[image_index]
+        current_image_label = labels[:, image_index]
+        network_output = logistic_unit_output(current_image, parameters_dictionary)
+        network_output = 1 if network_output >= 0.5 else 0
+        if network_output == current_image_label:
+            correct_predictions += 1
+            
+    accuracy = correct_predictions/num_of_images
+    return accuracy * 100
+            
 
 #%% Main Code
     
@@ -169,12 +205,16 @@ train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_datas
 #%% Example of a picture
 
 index = 13
+print('shape of train_set_x_orig: ', np.shape(train_set_x_orig))
 plt.imshow(train_set_x_orig[index])
 print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
 
 image = train_set_x_orig[index]
 
-parameters_dictionary = train_on_single_image(image, epochs=10000, learning_rate=1e-2, seed=1, true_output=1)
+parameters_dictionary = train_on_multiple_images(train_set_x_orig, train_set_y, epochs=1000, learning_rate=1e-2, seed=1, print_loss_flag=True)
 
-model_output = test_model(image, parameters_dictionary)
-print('Trained model output of image: ', model_output)
+model_accuracy = test_model_multiple_images(train_set_x_orig, train_set_y, parameters_dictionary)
+print('Trained model accuracy on training set: ', model_accuracy)
+
+model_accuracy = test_model_multiple_images(test_set_x_orig, test_set_y, parameters_dictionary)
+print('Trained model accuracy on test set: ', model_accuracy)
