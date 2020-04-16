ece9a473bc8e4304816999e04bfc658980d99c63
achoo486 <saliraza486@gmail.com>
2020-04-17 01:28:04 +0500

    [Nand2Tetris] Chapter 4 completed

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -1,14 +1,59 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(LOOP)
+//Store index i for looping in R0
+@8192
+D=A
+@R0
+M=D
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+//Starting point of screen
+@16384
+D=A
+@R1
+M=D
 
-// Put your code here.
+//Check if we need to black out screen or white it out
+@24576
+D=M
+@WHITE
+D;JEQ
+
+
+(BLACK)
+//Black out the pixel pointed by R1
+@R1
+A=M
+M=-1
+//update value of R1 to point to next pixel
+A=A+1
+D=A
+@R1
+M=D
+//decrement index i in R0 
+@R0
+M=M-1
+D=M
+@BLACK
+D;JNE
+
+@LOOP
+0;JMP
+
+(WHITE)
+//White out the pixel pointed by R1
+@R1
+A=M
+M=0
+//update value of R1 to point to next pixel
+A=A+1
+D=A
+@R1
+M=D
+//decrement index i in R0 
+@R0
+M=M-1
+D=M
+@WHITE
+D;JNE
+
+@LOOP
+0;JMP
--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -1,9 +1,34 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+//initialize product to zero
+@R2
+M=0
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//put index R1 in R3
+@R1
+D=M
+@R3
+M=D
 
-// Put your code here.
+(LOOP)
+//check if loop ended. if yes, then jump
+@R3
+D=M
+@END
+D;JEQ
+
+//loop not ended
+//calculation
+@R0
+D=M
+@R2
+M=M+D
+
+//decrement counter
+@R3
+M=M-1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
